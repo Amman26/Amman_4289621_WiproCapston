e014b0e3ddaac71dd56f8b96891e26c9101e9685
Amman <ammanfarooque@gmail.com>
2026-05-18 10:03:48 +0530

    till login page

--- Buy_Medicine_Module/conftest.py
+++ Buy_Medicine_Module/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 
 from utils.driver_factory import get_driver
+from utils.config_reader import read_config
+from pages.base_page import BasePage
+
+
+config = read_config()
+
+base_url = config.get("DEFAULT", "base_url")
 
 
 @pytest.fixture(scope="function")
@@ -8,7 +15,12 @@ def driver():
 
     driver = get_driver()
 
-    driver.get("https://www.apollo247.com/")
+    driver.get(base_url)
+
+    # Close promotional popup if present
+    base_page = BasePage(driver)
+
+    base_page.close_promotional_popup()
 
     yield driver
 
--- Buy_Medicine_Module/pages/base_page.py
+++ Buy_Medicine_Module/pages/base_page.py
@@ -3,58 +3,86 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
 
+from utils.config_reader import read_config
+
+
+config = read_config()
+
+timeout = int(config.get("DEFAULT", "timeout"))
+
 
 class BasePage:
 
     def __init__(self, driver):
+
         self.driver = driver
-        self.wait = WebDriverWait(driver, 20)
+
+        self.wait = WebDriverWait(driver, timeout)
 
     def click_element(self, locator):
+
         element = self.wait.until(
             EC.element_to_be_clickable(locator)
         )
+
         self.driver.execute_script(
             "arguments[0].click();",
             element
         )
 
     def enter_text(self, locator, text):
+
         element = self.wait.until(
             EC.visibility_of_element_located(locator)
         )
+
         element.clear()
         element.send_keys(text)
 
     def get_text(self, locator):
+
         return self.wait.until(
             EC.visibility_of_element_located(locator)
         ).text
 
     def is_element_visible(self, locator):
+
         try:
             return self.wait.until(
                 EC.visibility_of_element_located(locator)
             ).is_displayed()
+
         except TimeoutException:
             return False
 
     def close_promotional_popup(self):
-        """Attempts to close the Shadow DOM promotional popup if it exists."""
+
+        """Close promotional popup if present"""
+
         try:
+
             self.driver.implicitly_wait(2)
 
-            # Find the shadow host and pierce the shadow root
-            shadow_host = self.driver.find_element(By.TAG_NAME, "ct-web-popup-imageonly")
+            shadow_host = self.driver.find_element(
+                By.TAG_NAME,
+                "ct-web-popup-imageonly"
+            )
+
             shadow_root = shadow_host.shadow_root
 
-            # Find and click the close button inside
-            close_button = shadow_root.find_element(By.ID, "close")
+            close_button = shadow_root.find_element(
+                By.ID,
+                "close"
+            )
+
             close_button.click()
 
-            print("Promotional popup detected and closed.")
+            print("Popup closed successfully")
 
         except NoSuchElementException:
-            pass  # Ignore if popup isn't there
+
+            print("Popup not present")
+
         finally:
+
             self.driver.implicitly_wait(0)
